39634796ccf74483a4672e30ed8058994770da1e
pouria pou <pouriap024@yahoo.com>
2024-05-20 20:50:31 +0200

    adjust atlas connection string for backend

--- src/DB/index.js
+++ src/DB/index.js
@@ -1,26 +1,20 @@
 import mongoose from "mongoose";
 
 const MONGO_URI =
-  process.env.MONGO_URI ||
+  process.env.MONGODB_URI ||
   `mongodb+srv://ppourgoshtasbi:pc71KxH71oXrIkNM@cluster0.xxfjrqm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-const client = new MongoClient(MONGO_URI, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
-
+const clientOptions = {
+  serverApi: { version: "1", strict: true, deprecationErrors: true },
+};
 export const setConnection = async () => {
   try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
+    await mongoose.connect(MONGO_URI, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    await mongoose.disconnect();
   }
 };
