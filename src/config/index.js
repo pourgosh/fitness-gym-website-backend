b7fb384372a827629f4ac36054d8700a45f26652
pouria pou <pouriap024@yahoo.com>
2024-05-21 08:50:49 +0200

    remove extra "/" from cors origin

--- src/config/index.js
+++ src/config/index.js
@@ -7,7 +7,7 @@ export const appConfig = (app) => {
   app.set("trust proxy", 1);
   app.use(
     cors({
-      origin: "https://mutant-fitness.netlify.app/",
+      origin: "https://mutant-fitness.netlify.app",
       optionsSuccessStatus: 200,
     })
   );
