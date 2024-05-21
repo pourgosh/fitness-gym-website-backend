5221ee1a1062f06db06a21772bac9e57f7c9649e
pouria pou <pouriap024@yahoo.com>
2024-05-21 10:48:56 +0200

    adjust origin url

--- src/config/index.js
+++ src/config/index.js
@@ -7,7 +7,7 @@ export const appConfig = (app) => {
   app.set("trust proxy", 1);
   app.use(
     cors({
-      origin: "https://mutant-fitness.netlify.app/",
+      origin: "https://mutant-fitness.netlify.app",
       optionsSuccessStatus: 200,
       methods: ["POST", "GET", "PUT", "DELETE"],
       credentials: true,
@@ -17,7 +17,7 @@ export const appConfig = (app) => {
   app.options(
     "*",
     cors({
-      origin: "https://mutant-fitness.netlify.app/",
+      origin: "https://mutant-fitness.netlify.app",
       methods: ["GET", "POST", "PUT", "DELETE"],
       credentials: true,
     })
