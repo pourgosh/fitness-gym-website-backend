48cc5fca508d16e0519fe24fc83803387989bf57
pouria pou <pouriap024@yahoo.com>
2024-05-21 11:04:59 +0200

    remove cors options

--- src/config/index.js
+++ src/config/index.js
@@ -5,23 +5,7 @@ import cookieParser from "cookie-parser";
 
 export const appConfig = (app) => {
   app.set("trust proxy", 1);
-  app.use(
-    cors({
-      origin: "https://mutant-fitness.netlify.app",
-      optionsSuccessStatus: 200,
-      methods: ["POST", "GET", "PUT", "DELETE"],
-      credentials: true,
-    })
-  );
-  // Handle preflight requests
-  app.options(
-    "*",
-    cors({
-      origin: "https://mutant-fitness.netlify.app",
-      methods: ["GET", "POST", "PUT", "DELETE"],
-      credentials: true,
-    })
-  );
+  app.use(cors());
   app.use(express.json());
   app.use(logger("dev"));
   app.use(express.urlencoded({ extended: false }));
